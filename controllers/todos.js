45c86c37773bca0f6c4c8a0fa62e8e8b8760b19c
daisong <daisong1860@hotmail.com>
2017-09-02 15:47:28 +0800

    FEAT:完成todo中的done功能

--- controllers/todos.js
+++ controllers/todos.js
@@ -1,30 +1,27 @@
 const todoItem = require('../model').todoItem
-
-var todoList = []
-
-var todoNum = 0
-var doneNum = 0
+const moment = require('moment')
+moment.locale('zh-cn')
 
 exports.create = function (req, res) {
   var newTodo = req.body.data
   todoItem.create({
     typeId: newTodo.type,
     context: newTodo.context,
-    status: 0,
+    status: false,
     finishedAt: 0
   }).then(item => {
-    todoNum = 0
-    doneNum = 0
     todoItem.findAll().then(todos => {
       var ans = {}
+      var todoNum = 0
+      var doneNum = 0
       todos.forEach(obj => {
-        if (obj.status === 1) {
+        obj.createdAt = moment(obj.createdAt).fromNow()
+        if (obj.status) {
           doneNum++
         } else {
           todoNum++
         }
       })
-      todoList = todos
       ans.todoNum = todoNum
       ans.doneNum = doneNum
       ans.list = todos
@@ -34,18 +31,18 @@ exports.create = function (req, res) {
 }
 
 exports.list = function (req, res) {
-  todoNum = 0
-  doneNum = 0
   todoItem.findAll().then(todos => {
     var ans = {}
+    var todoNum = 0
+    var doneNum = 0
     todos.forEach(obj => {
-      if (obj.status === 1) {
+      obj.createdAt = moment(obj.createdAt).fromNow()
+      if (obj.status) {
         doneNum++
       } else {
         todoNum++
       }
     })
-    todoList = todos
     ans.todoNum = todoNum
     ans.doneNum = doneNum
     ans.list = todos
@@ -54,98 +51,32 @@ exports.list = function (req, res) {
 }
 
 exports.done = function (req, res) {
-  var doneId = req.body.data
-  // todoItem.findById(req.body.data, {
-  //   include: [{
-  //     model: TodoItem,
-  //     as: 'todoItems',
-  //   }],
-  // })
-  // .then(todo => {
-  //   if (!todo) {
-  //     return res.status(404).send({
-  //       message: 'Todo Not Found',
-  //     });
-  //   }
-  //   return todo
-  //     .update({
-  //       title: req.body.title || todo.title,
-  //     })
-  //     .then(() => res.status(200).send(todo))
-  //     .catch((error) => res.status(400).send(error));
-  // })
-  // .catch((error) => res.status(400).send(error));
-
-  todoList.forEach(obj => {
-    if (obj.id === doneId) {
-      obj.done = true
-      doneNum++
-      todoNum--
+  todoItem.findById(req.body.data).then(todo => {
+    if (!todo) {
+      return res.status(404).send({
+        message: 'Todo Not Found'
+      })
     }
-  })
-  var ans = {}
-  ans.todoNum = todoNum
-  ans.doneNum = doneNum
-  ans.list = todoList
-  res.send(ans)
+    todo.update({
+      status: true
+    }).then(() => {
+      todoItem.findAll().then(todos => {
+        var ans = {}
+        var todoNum = 0
+        var doneNum = 0
+        todos.forEach(obj => {
+          obj.createdAt = moment(obj.createdAt).fromNow()
+          if (obj.status) {
+            doneNum++
+          } else {
+            todoNum++
+          }
+        })
+        ans.todoNum = todoNum
+        ans.doneNum = doneNum
+        ans.list = todos
+        res.send(ans)
+      })
+    })
+  }).catch((error) => res.status(400).send(error))
 }
-
-// module.exports = {
-  // retrieve(req, res) {
-  //   return Todo
-  //     .findById(req.params.todoId, {
-  //       include: [{
-  //         model: TodoItem,
-  //         as: 'todoItems',
-  //       }],
-  //     })
-  //     .then((todo) => {
-  //       if (!todo) {
-  //         return res.status(404).send({
-  //           message: 'Todo Not Found',
-  //         });
-  //       }
-  //       return res.status(200).send(todo);
-  //     })
-  //     .catch((error) => res.status(400).send(error));
-  // },
-  // update(req, res) {
-  //   return Todo
-  //     .findById(req.params.todoId, {
-  //       include: [{
-  //         model: TodoItem,
-  //         as: 'todoItems',
-  //       }],
-  //     })
-  //     .then(todo => {
-  //       if (!todo) {
-  //         return res.status(404).send({
-  //           message: 'Todo Not Found',
-  //         });
-  //       }
-  //       return todo
-  //         .update({
-  //           title: req.body.title || todo.title,
-  //         })
-  //         .then(() => res.status(200).send(todo))
-  //         .catch((error) => res.status(400).send(error));
-  //     })
-  //     .catch((error) => res.status(400).send(error));
-  // },
-  // destroy(req, res) {
-  //   return Todo
-  //     .findById(req.params.todoId)
-  //     .then(todo => {
-  //       if (!todo) {
-  //         return res.status(400).send({
-  //           message: 'Todo Not Found',
-  //         });
-  //       }
-  //       return todo
-  //         .destroy()
-  //         .then(() => res.status(204).send())
-  //         .catch((error) => res.status(400).send(error))
-  //     })
-  //     .catch((error) => res.status(400).send(error))
-  // },
-// }

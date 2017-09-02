77a5bf0838d02478dd4d5c29a816312313f99c50
daisong <daisong1860@hotmail.com>
2017-09-02 16:11:16 +0800

    FEAT:完成todos delete功能

--- controllers/todos.js
+++ controllers/todos.js
@@ -2,35 +2,7 @@ const todoItem = require('../model').todoItem
 const moment = require('moment')
 moment.locale('zh-cn')
 
-exports.create = function (req, res) {
-  var newTodo = req.body.data
-  todoItem.create({
-    typeId: newTodo.type,
-    context: newTodo.context,
-    status: false,
-    finishedAt: 0
-  }).then(item => {
-    todoItem.findAll().then(todos => {
-      var ans = {}
-      var todoNum = 0
-      var doneNum = 0
-      todos.forEach(obj => {
-        obj.createdAt = moment(obj.createdAt).fromNow()
-        if (obj.status) {
-          doneNum++
-        } else {
-          todoNum++
-        }
-      })
-      ans.todoNum = todoNum
-      ans.doneNum = doneNum
-      ans.list = todos
-      res.send(ans)
-    })
-  })
-}
-
-exports.list = function (req, res) {
+function findAll (req, res) {
   todoItem.findAll().then(todos => {
     var ans = {}
     var todoNum = 0
@@ -49,6 +21,21 @@ exports.list = function (req, res) {
     res.send(ans)
   })
 }
+exports.create = function (req, res) {
+  var newTodo = req.body.data
+  todoItem.create({
+    typeId: newTodo.type,
+    context: newTodo.context,
+    status: false,
+    finishedAt: 0
+  }).then(item => {
+    findAll(req, res)
+  })
+}
+
+exports.list = function (req, res) {
+  findAll(req, res)
+}
 
 exports.done = function (req, res) {
   todoItem.findById(req.body.data).then(todo => {
@@ -60,23 +47,20 @@ exports.done = function (req, res) {
     todo.update({
       status: true
     }).then(() => {
-      todoItem.findAll().then(todos => {
-        var ans = {}
-        var todoNum = 0
-        var doneNum = 0
-        todos.forEach(obj => {
-          obj.createdAt = moment(obj.createdAt).fromNow()
-          if (obj.status) {
-            doneNum++
-          } else {
-            todoNum++
-          }
-        })
-        ans.todoNum = todoNum
-        ans.doneNum = doneNum
-        ans.list = todos
-        res.send(ans)
-      })
+      findAll(req, res)
     })
   }).catch((error) => res.status(400).send(error))
+}
+
+exports.delete = function (req, res) {
+  todoItem.findById(req.body.data).then(todo => {
+    if (!todo) {
+      return res.status(404).send({
+        message: 'Todo Not Found'
+      })
+    }
+    todo.destroy().then(
+      () => findAll(req, res)).catch(
+      (error) => res.status(400).send(error))
+  })
 }
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ var express = require('express')
 const todosController = require('../controllers/todos')
 var router = express.Router()
 
-/* GET todos listing. */
 router.get('/todos/list', todosController.list)
 router.post('/todos/done', todosController.done)
 router.post('/todos/add', todosController.create)
+router.post('/todos/delete', todosController.delete)
 
 module.exports = router

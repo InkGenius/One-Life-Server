6f3cc1f1e129ad59597307f87bbb35a55225010e
daisong <daisong1860@hotmail.com>
2017-09-02 11:16:50 +0800

    FEAT:完成添加代办功能

--- controllers/todos.js
+++ controllers/todos.js
@@ -5,62 +5,92 @@ var todoList = []
 var todoNum = 0
 var doneNum = 0
 
-module.exports = {
-  create (req, res) {
-    var newTodo = req.body.data
-    return todoItem.create({
-      typeId: newTodo.typeId,
-      context: newTodo.context,
-      status: 0,
-      finishedAt: 0
-    }).then((todo) => res.status(201).send(this.list(req, res)))
-      .catch((error) => res.status(400).send(error))
-  },
-  findAll (req, res) {
-    return todoItem.findAll({
-      include: [{
-        model: todoItem,
-        as: 'todoItems'
-      }],
-      order: [
-        ['createdAt', 'DESC']
-      ]
-    })
-    .then((todos) => res.status(200).send(todos))
-    .catch((error) => res.status(400).send(error))
-  },
-  list (req, res) {
-    todoList = this.findAll(req, res)
+exports.create = function (req, res) {
+  var newTodo = req.body.data
+  todoItem.create({
+    typeId: newTodo.type,
+    context: newTodo.context,
+    status: 0,
+    finishedAt: 0
+  }).then(item => {
     todoNum = 0
     doneNum = 0
-    todoList.forEach(obj => {
+    todoItem.findAll().then(todos => {
+      var ans = {}
+      todos.forEach(obj => {
+        if (obj.status === 1) {
+          doneNum++
+        } else {
+          todoNum++
+        }
+      })
+      todoList = todos
+      ans.todoNum = todoNum
+      ans.doneNum = doneNum
+      ans.list = todos
+      res.send(ans)
+    })
+  })
+}
+
+exports.list = function (req, res) {
+  todoNum = 0
+  doneNum = 0
+  todoItem.findAll().then(todos => {
+    var ans = {}
+    todos.forEach(obj => {
       if (obj.status === 1) {
         doneNum++
       } else {
         todoNum++
       }
     })
-    var ans = {}
+    todoList = todos
     ans.todoNum = todoNum
     ans.doneNum = doneNum
-    ans.list = todoList
+    ans.list = todos
     res.send(ans)
-  },
-  done (req, res) {
-    var doneId = req.body.data
-    todoList.forEach(obj => {
-      if (obj.id === doneId) {
-        obj.done = true
-        doneNum++
-        todoNum--
-      }
-    })
-    var ans = {}
-    ans.todoNum = todoNum
-    ans.doneNum = doneNum
-    ans.list = todoList
-    res.send(ans)
-  }
+  })
+}
+
+exports.done = function (req, res) {
+  var doneId = req.body.data
+  // todoItem.findById(req.body.data, {
+  //   include: [{
+  //     model: TodoItem,
+  //     as: 'todoItems',
+  //   }],
+  // })
+  // .then(todo => {
+  //   if (!todo) {
+  //     return res.status(404).send({
+  //       message: 'Todo Not Found',
+  //     });
+  //   }
+  //   return todo
+  //     .update({
+  //       title: req.body.title || todo.title,
+  //     })
+  //     .then(() => res.status(200).send(todo))
+  //     .catch((error) => res.status(400).send(error));
+  // })
+  // .catch((error) => res.status(400).send(error));
+
+  todoList.forEach(obj => {
+    if (obj.id === doneId) {
+      obj.done = true
+      doneNum++
+      todoNum--
+    }
+  })
+  var ans = {}
+  ans.todoNum = todoNum
+  ans.doneNum = doneNum
+  ans.list = todoList
+  res.send(ans)
+}
+
+// module.exports = {
   // retrieve(req, res) {
   //   return Todo
   //     .findById(req.params.todoId, {
@@ -118,4 +148,4 @@ module.exports = {
   //     })
   //     .catch((error) => res.status(400).send(error))
   // },
-}
+// }

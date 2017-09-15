1c90c9bd47858ef62e79ce68e829ea1c092b0e4f
daisong <daisong1860@hotmail.com>
2017-09-15 11:39:20 +0800

    FEAT:添加record路由和控制器

---
+++ controllers/records.js
@@ -0,0 +1,68 @@
+const todoItem = require('../model').todoItem
+const moment = require('moment')
+moment.locale('zh-cn')
+
+function findAll (req, res) {
+  todoItem.findAll().then(todos => {
+    var ans = {}
+    var todoNum = 0
+    var doneNum = 0
+    todos.forEach(obj => {
+      obj.createdAt = moment(obj.createdAt).fromNow()
+      if (obj.status) {
+        doneNum++
+      } else {
+        todoNum++
+      }
+    })
+    ans.todoNum = todoNum
+    ans.doneNum = doneNum
+    ans.list = todos
+    res.send(ans)
+  })
+}
+exports.create = function (req, res) {
+  var newTodo = req.body.data
+  console.log(newTodo)
+  // todoItem.create({
+  //   typeId: newTodo.type,
+  //   context: newTodo.context,
+  //   status: false,
+  //   finishedAt: 0
+  // }).then(item => {
+  //   findAll(req, res)
+  // })
+}
+
+exports.list = function (req, res) {
+  findAll(req, res)
+}
+
+exports.done = function (req, res) {
+  todoItem.findById(req.body.data).then(todo => {
+    if (!todo) {
+      return res.status(404).send({
+        message: 'Todo Not Found'
+      })
+    }
+    todo.update({
+      status: true,
+      finishedAt: Date.now()
+    }).then(() => {
+      findAll(req, res)
+    })
+  }).catch((error) => res.status(400).send(error))
+}
+
+exports.delete = function (req, res) {
+  todoItem.findById(req.body.data).then(todo => {
+    if (!todo) {
+      return res.status(404).send({
+        message: 'Todo Not Found'
+      })
+    }
+    todo.destroy().then(
+      () => findAll(req, res)).catch(
+      (error) => res.status(400).send(error))
+  })
+}
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 const todosController = require('../controllers/todos')
+const recordsController = require('../controllers/records')
 var router = express.Router()
 
 router.get('/todos/list', todosController.list)
@@ -7,4 +8,5 @@ router.post('/todos/done', todosController.done)
 router.post('/todos/add', todosController.create)
 router.post('/todos/delete', todosController.delete)
 
+router.post('/record/add', recordsController.create)
 module.exports = router
